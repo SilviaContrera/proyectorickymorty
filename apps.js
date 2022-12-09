39395407eda01dbac0be054afad232f86a8228a9
silvia <silviaecontrera7@gmail.com>
2022-12-09 14:49:55 -0300

    p

---
+++ apps.js
@@ -0,0 +1,25 @@
+const contenedor = document.getElementById("contenedor-tarjetas");
+console.log(contenedor);
+
+fetch("https://rickandmortyapi.com/api/character")
+    .then(respuesta => respuesta.json())
+    .then(data => {
+        const dataApi = data.results
+        console.log(dataApi)
+
+
+        let apiRickyMorty = ""
+        for (let i = 0; i < dataApi.length; i++) {
+            apiRickyMorty = apiRickyMorty + `
+        <div class = "card">
+        <div class = "container-img">
+        <img src= ${dataApi[i].image} alt=${dataApi[i].name}>
+        </div>
+        <p>
+        ${dataApi[i].name}
+        </p>
+        </div>
+        `
+        }
+        contenedor.innerHTML = apiRickyMorty
+    })
